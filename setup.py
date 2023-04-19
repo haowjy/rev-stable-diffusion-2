61ebc76a05f0dda37cd2765825a35512774273a2
Jimmy Yao <jinghaoyao@gmail.com>
2023-04-19 08:53:14 -0400

    edti setup

--- setup.py
+++ setup.py
@@ -6,15 +6,12 @@ LONG_DESCRIPTION = 'My first Python package with a slightly longer description'
 
 # Setting up
 setup(
-       # the name must match the folder name 'verysimplemodule'
         name="samgit", 
         version=VERSION,
-        author="Jason Dsouza",
-        author_email="<youremail@email.com>",
+        author="Jimmy Yao",
+        author_email="jinghaoyao@gmail.com",
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
         packages=find_packages(),
         install_requires=open("requirements.txt").read().splitlines(), # add any additional packages that 
-        # needs to be installed along with your package. Eg: 'caer'
-        keywords=['python', 'first package'],
 )
